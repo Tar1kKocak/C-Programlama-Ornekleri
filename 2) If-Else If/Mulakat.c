4ac41fd8f2c4c7e2442875974d6f4bbfe58c5f82
Tarık <116958073+Tar1kKocak@users.noreply.github.com>
2022-10-29 23:31:26 +0300

    Add files via upload

---
+++ 2) If-Else If/Mulakat.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+ {
+     // Yazilimin amaci : Cinsiyet ve boy'a göre degiskenlik gosteren mulakat.
+
+	setlocale(LC_ALL,"Turkish");
+
+	char cinsiyet;
+	int boy;
+
+	printf("Lütfen cinsiyetinizi giriniz E/K.\n");
+	scanf("%c",&cinsiyet);
+	printf("Lütfen boyunuzu cm cinsinden giriniz.\n");
+	scanf("%d",&boy);
+
+
+	if (cinsiyet == 'E' || cinsiyet == 'e' && boy >= 160) {
+		printf ("Tebrikler mülakatý geçtiniz.");
+	}
+	else if (cinsiyet == 'K' || cinsiyet == 'k' && boy >= 150){
+
+		printf("Tebrikler mülakatý geçtiniz.");
+	}
+	else {
+		printf("Mülakatý geçemediniz.");
+	}
+	return 0;
+}
