209cd40000508cb223c96ceb954fe8d0f0a95f7a
Tarık <116958073+Tar1kKocak@users.noreply.github.com>
2022-10-30 21:49:26 +0300

    Add files via upload

---
+++ 5) For Dongusu/Sekil Olusturma.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale (LC_ALL,"Turkish");
+
+    int sayi,satir,sutun;
+
+    printf("Lütfen bir sayý giriniz.\n");
+    scanf("%d",&sayi);
+
+
+    for (satir=1;satir<=sayi;satir++) {
+        for (sutun=1;sutun<=satir;sutun++){
+            printf("*");
+
+        }
+        printf("\n");
+
+
+
+    }
+        for (satir=1;satir<=sayi;satir++) {
+
+            for (sutun=sayi-satir;sutun>=0;sutun--){
+                    printf("*");
+
+        }
+            printf("\n");
+        }
+
+    return 0;
+}
