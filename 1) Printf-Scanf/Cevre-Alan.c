4cd1e3791c329ac3f1a17287dc0ece8d2b6976ad
Tarık <116958073+Tar1kKocak@users.noreply.github.com>
2022-10-31 19:30:52 +0300

    Update Cevre-Alan.c

--- 1) Printf-Scanf/Cevre-Alan.c
+++ 1) Printf-Scanf/Cevre-Alan.c
@@ -11,22 +11,22 @@
  	setlocale(LC_ALL,"Turkish");
  	int kareninbiruzunluk;
 
- 	printf("Alanýný ve çevresini bulmak istediðiniz karenin bir kenar uzunluðunu belirtiniz...\n");
+ 	printf("Alanini ve cevresini bulmak istediginiz karenin bir kenar uzunlugunu belirtiniz...\n");
  	scanf("%d",&kareninbiruzunluk);
 
  	int dikdortgenkisakenar,dikdortgenuzunkenar;
 
- 	printf("Alanýný ve çevresini bulmak istediðiniz dikdörtgenin uzun kenarýný giriniz.\n");
+ 	printf("Alanini ve cevresini bulmak istediginiz dikdörtgenin uzun kenarini giriniz.\n");
  	scanf("%d",&dikdortgenuzunkenar);
 
- 	printf("Alanýný ve çevresini bulmak istediðiniz dikdörtgenin kýsa kenarýný giriniz.\n");
+ 	printf("Alanini ve cevresini bulmak istediginiz dikdörtgenin kisa  kenarini giriniz.\n");
  	scanf("%d",&dikdortgenkisakenar);
 
 
- 	printf("Karenin çevresi : %d\n",kareninbiruzunluk * 4);
- 	printf("Karenin alaný : %d\n",kareninbiruzunluk * kareninbiruzunluk);
- 	printf("Dikdörtgenin çevresi : %d\n",dikdortgenkisakenar * 2 + dikdortgenuzunkenar * 2);
- 	printf("Dikdörtgenin alaný : %d\n",dikdortgenkisakenar*dikdortgenuzunkenar);
+ 	printf("Karenin cevresi : %d\n",kareninbiruzunluk * 4);
+ 	printf("Karenin alani : %d\n",kareninbiruzunluk * kareninbiruzunluk);
+ 	printf("Dikdörtgenin cevresi : %d\n",dikdortgenkisakenar * 2 + dikdortgenuzunkenar * 2);
+ 	printf("Dikdörtgenin alani : %d\n",dikdortgenkisakenar*dikdortgenuzunkenar);
 
 
 
