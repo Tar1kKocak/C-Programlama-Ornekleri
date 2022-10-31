75c2e2e6d827f6e2ff214dd6495d086d88236303
Tarık <116958073+Tar1kKocak@users.noreply.github.com>
2022-10-31 23:15:32 +0300

    Add files via upload

---
+++ 5) For Dongusu/Carpma Ama Toplama.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int sayi1,sayi2,i,sonuc=0;
+
+    printf("Carpilmasini istediginiz iki adet pozitif tam sayi giriniz.\n");
+    scanf("%d%d",&sayi1,&sayi2);
+
+    for (i=sayi1;i>=1;i--) {
+
+            sonuc += sayi2;
+
+    }
+
+ printf ("%d",sonuc);
+
+    return 0;
+}
