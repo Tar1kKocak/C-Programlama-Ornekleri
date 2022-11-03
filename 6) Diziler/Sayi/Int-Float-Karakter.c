ea1b70aafa114503fb8551596579331a7a6f2ca4
Tarık <116958073+Tar1kKocak@users.noreply.github.com>
2022-11-03 16:58:05 +0300

    Add files via upload

---
+++ 6) Diziler/Sayi/Int-Float-Karakter.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+        //Ýnt Dizileri :
+
+    int b[6]={},k;
+
+    for(k=0;k<5;k++) {
+        printf("Lutfen bir tam sayi giriniz.\n");
+        scanf("%d",&b[k]);
+    }
+    for(k=0;k<5;k++) {
+        printf("%d \n",b[k]);
+    }
+
+
+
+        //Float Dizileri :
+
+    int i;
+    float sayilar[7]={};
+    for(i=0;i<7;i++) {
+        printf("Lutfen bir sayi giriniz.\n");
+        scanf("%f",&sayilar[i]);
+    }
+
+    for(i=0;i<7;i++) {
+        printf("%.2f\n",sayilar[i]);
+    }
+
+
+
+
+        //Karakter Dizileri :
+
+     char ulke[6];
+
+    for (int a=0;a<5;a++) {
+        printf("Lutfen bir karakter giriniz.\n");
+        scanf(" %c",&ulke[a]);
+    }
+        printf("%s",ulke);
+
+    return 0;
+}
