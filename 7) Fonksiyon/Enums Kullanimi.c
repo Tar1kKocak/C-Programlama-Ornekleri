270bc358a1ccde9c958121f63ddc10fefc35885a
Tarık <116958073+Tar1kKocak@users.noreply.github.com>
2022-11-15 20:24:25 +0300

    Add files via upload

---
+++ 7) Fonksiyon/Enums Kullanimi.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+    //Yazilimin Amaci : Enumeration kullanimi
+enum hafta{Pazartesi=1,Sali,Carsamba,Persembe,Cuma,Cumartesi,Pazar};
+
+enum aylar{Ocak=1,Mart=3,Haziran=6,Eylul=9};
+
+int main()
+{
+
+    printf("%d\n",Carsamba);
+    printf("%d\n",Ocak);
+    printf("%d\n",Mart);
+    printf("%d\n",Haziran);
+    printf("%d\n",Eylul);
+
+
+    return 0;
+}
