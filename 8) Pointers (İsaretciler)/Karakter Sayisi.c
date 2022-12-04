93ef1100fe07f74428836f24dc9cdf0d437d21a7
Tarık <116958073+Tar1kKocak@users.noreply.github.com>
2022-12-04 21:34:19 +0300

    Add files via upload

---
+++ 8) Pointers (İsaretciler)/Karakter Sayisi.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_SIZE 100
+
+//Yazilimin Amaci : Girilen metnin karakter sayisini bulmak.
+
+int uzunlukhesapla(char *karakterdizisiadresi){
+    int uzunluk=0;
+    int i;
+
+    for (i=0;karakterdizisiadresi[i]!='\0';i++){
+
+        uzunluk++;
+    }
+    return uzunluk;
+}
+
+int main()
+{
+    char karakterdizisi[MAX_SIZE];
+
+    printf("Lutfen uzunlugunu ogrenmek istediginiz kelimeyi veya cumleyi giriniz.\n");
+    gets(karakterdizisi);
+
+    printf("%d",uzunlukhesapla(karakterdizisi));
+
+    return 0;
+}
